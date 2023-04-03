5077415519f24c99e61ecc7dfa37c1d284b28a08
Tomson <tomas.borcuch@gmail.com>
2023-04-03 11:42:28 +0200

    update loop

--- loop.js
+++ loop.js
@@ -1,4 +1,10 @@
-int =1
-for(i; i<10; i++){
+console.log("Hej Przeliczmy od 10 w dol i w gore")
+i=0
+for(i; i<=10; i++){
+    console.log(i)
+}
+console.log("\n")
+
+for(i=10;i>=0;i--){
     console.log(i)
 }
